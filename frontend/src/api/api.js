883c376ffd6baa636d67aff2761ac824bc2966cd
shubham kumar <shubhamraj9493@gmail.com>
2026-09-13 23:24:33 +0530

    Fix syntax error in API baseURL definition

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: baseURL: 'https://blue-signal-backend.onrender.com/api',
+  baseURL: 'https://blue-signal-backend.onrender.com/api',
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
