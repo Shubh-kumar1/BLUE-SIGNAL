1aeed0406b8cdee6b45410f4b5bf2d78997ad118
shubham kumar <shubhamraj9493@gmail.com>
2026-09-13 23:24:13 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: baseURL: 'https://blue-signal-backend.onrender.com/api',
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
